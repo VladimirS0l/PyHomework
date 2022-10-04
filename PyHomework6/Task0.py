38d7df2691f2d82758701dfe17380ec509053f1d
VladimirS0l <vbnm1993@yandex.ru>
2022-10-04 16:33:21 +0300

    red3

--- PyHomework6/Task0.py
+++ PyHomework6/Task0.py
@@ -12,3 +12,61 @@
 # (2+((5-3)*(16-14)))/3 => 2
 # (256 - 194 => некорректная запись скобок
 
+
+def list_numbers(expr):
+    list_number = []
+    expr = expr + ' '
+    temp = ''
+    for i in expr:
+        if i.isdigit():
+            temp += i
+        else:
+            list_number.append(temp)
+            temp = ''
+    list_number = list(filter(lambda x: x != '', list_number))
+    list_number = list(map(float, list_number))
+    return list_number
+
+
+def list_operations(expr):
+    list_operation = list(filter(lambda x: x == '/' or x == '*' or x == '+' or x == '-', expr))
+    return list_operation
+
+
+def list_reduce(i, list1, list2):
+    del list1[i + 1]
+    del list2[i]
+    return list1, list2
+
+
+def expr_operation(list_number, list_operation):
+    while len(list_operation) > 0:
+        while '/' in list_operation:
+            for i, elem in enumerate(list_operation):
+                if elem == '/':
+                    list_number[i] = list_number[i] / list_number[i+1]
+                    list_reduce(i, list_number, list_operation)
+        while '*' in list_operation:
+            for i, elem in enumerate(list_operation):
+                if elem == '*':
+                    list_number[i] = list_number[i] * list_number[i+1]
+                    list_reduce(i, list_number, list_operation)
+        while '+' in list_operation:
+            for i, elem in enumerate(list_operation):
+                if elem == '+':
+                    list_number[i] = list_number[i] + list_number[i+1]
+                    list_reduce(i, list_number, list_operation)
+        while '-' in list_operation:
+            for i, elem in enumerate(list_operation):
+                if elem == '-':
+                    list_number[i] = list_number[i] - list_number[i+1]
+                    list_reduce(i, list_number, list_operation)
+    return list_number[0]
+
+
+expr_to_calc = '3-4*2' #input('Введите выражение для расчета: ')
+list_number = list_numbers(expr_to_calc)
+list_operation = list_operations(expr_to_calc)
+print(f'{expr_to_calc} ->\n{expr_operation(list_number, list_operation)}')
+
+print(eval('3-4*2'))

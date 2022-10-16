19310cdf71a9cc5ccb17930d4f35769bb8d7d492
VladimirS0l <vbnm1993@yandex.ru>
2022-10-16 15:13:52 +0300

    py5 red2

--- PyHomework5/Task2.py
+++ PyHomework5/Task2.py
@@ -7,101 +7,163 @@
 import random
 
 print('\t\tПРИВЕТ - ЭТА ИГРА КТО ВЫТЯНЕТ ПОСЛЕДНЮЮ КОНФЕТУ')
+print('Игрок 1')
 
-name = input('Введите ваше имя: ')
 
-def check():
+def check_name_run():
     while True:
-        x = int(input('\tЕсли хочешь сыграть с ботом нажми 1, с другим человеком нажми 2: ')) 
-        if x == 1 or x == 2:   
-            return x
+        name = input('Введите ваше имя: ')
+        if name.isalpha():
+            return name
         else:
-            print('Неверный ввод')
-choose = check()
+            print('Вы ввели цифры')
 
-#choose = int(input('\tЕсли хочешь сыграть с ботом нажми 1, с другим человеком нажми 2: '))
 
-'''Игра с ботом'''
+name1 = check_name_run()
+
+
+def check_sweet():
+    while True:
+        sweet = input('Введите максимальное количество конфет: ')
+        if sweet.isdigit():
+            s = int(sweet)
+            if 30 < s <= 1000:
+                return s
+        else:
+            print('Некорректный ввод')
+
+
+sweet = check_sweet()
+
+
+def check_onestep():
+    while True:
+        onestep = input(
+            'Введите максимальное количество конфет, которые будем брать за один шаг: ')
+        if onestep.isdigit():
+            os = int(onestep)
+            if 0 < os <= (sweet * 0.4):
+                return os
+        else:
+            print('Некорректный ввод')
+
+
+one_step = check_onestep()
+
+
+def player_step_check():
+    while True:
+        player_step = input(
+            f'Сколько конфет вы ходите взять, можно взять от 1 до {one_step}: ')
+        if player_step.isdigit():
+            ps = int(player_step)
+            if 0 < ps <= one_step:
+                return ps
+        else:
+            print('Некорректный ввод')
+
+
+# Выбор режима игры (проверка на ввод режима игры)
+
+print('Очень хорошо, теперь выбери режим игры, \n1 - игра с ботом, \n2 - игра с человеком, \n3 - выход')
+
+
+def check_choose():
+    while True:
+        choose = input('Введите номер режима: ')
+        if choose.isdigit():
+            ch = int(choose)
+            if 0 < ch < 4:
+                return ch
+        else:
+            print('Некорректный ввод')
+
+
+choose = check_choose()
+
+
 if choose == 1:
     print('Вы выбрали режим игры с ботом')
-    sweet = int(input('Введите сколько будет конфет на столе: '))
-    one_step = int(input('Введите сколько конфет можно взять за раз: '))
-    first_step = True if random.randint(1,2) == 1 else False
-
-    while sweet > 0:
-        if first_step == True:
-            if sweet >= 5:
-                bot_step = random.randint(1, one_step)
-                sweet -= bot_step
-                print(f"Бот взял {bot_step} конфет, осталось {sweet}")
-            elif sweet < 5:
-                bot_step = 1
-                sweet -= bot_step
-                print(f"Бот взял {bot_step} конфет, осталось {sweet}")
-                if sweet == 0:
-                    print('Бот проиграл!')
-                    break
-            player_step = int(input(f'Введите сколько конфет вы возьмете от 1 до {one_step}: '))
-            sweet -=player_step
-            print(f"Игрок взял {player_step} конфет, осталось {sweet}")
-            if sweet == 0:
-                print(f'{name} проиграл(a)!')
+    player1 = name1
+    player2 = 'Бот'
+    players = [player1, player2]
+    random.shuffle(players)
+    player1, player2 = players
+    if player1 == 'Бот':
+        while sweet > 0:
+            if sweet < one_step+2:
+                player1_step = sweet - 1
+            else:
+                player1_step = sweet - (one_step + 2) * (sweet//(one_step + 2))
+            sweet -= player1_step
+            print(f'{player1} взял {player1_step} конфет, всего осталось {sweet}')
+            if sweet <= 0:
+                print(f'{player2} выйграл')
                 break
-        else:
-            player_step = int(input(f'Введите сколько конфет вы возьмете от 1 до {one_step}: '))
-            sweet -= player_step
-            print(f"Игрок взял {player_step} конфет, осталось {sweet}")
-            if sweet == 1:
-                print(f'{name} проиграл(a)!')
+            player2_step = player_step_check()
+            sweet -= player2_step
+            print(f"{player2} взял {player2_step} конфет, всего осталось {sweet}")
+            if sweet <= 0:
+                print(f'{player1} выйграл')
                 break
-            if sweet >= 5:
-                bot_step = random.randint(1, one_step)
-                sweet -= bot_step
-                print(f"Бот взял {bot_step} конфет, осталось {sweet}")
-            elif sweet < 5:
-                bot_step = 1
-                sweet -= bot_step
-                print(f"Бот взял {bot_step} конфет, осталось {sweet}")
-                if sweet == 0:
-                    print('Бот проиграл!')
-                    break
-
-'''Игра с другим игроком'''
-if choose == 2:
-    print('Вы выбрали режим игры с другим игроком')
-    name2 = input('Введите имя второго игрока: ')
-    sweet = int(input('Введите сколько будет конфет на столе: '))
-    one_step = int(input('Введите сколько конфет можно взять за раз: '))
-    first_step = True if random.randint(1,2) == 1 else False
-
-    while sweet > 0:
-        if first_step == True:
-            player1_step = int(input(f'{name} сколько конфет вы возьмете от 1 до {one_step}: '))
-            sweet -=player1_step
-            print(f"{name} взял {player1_step} конфет, осталось {sweet}")
-            if sweet == 0:
-                print(f'{name} проиграл(a)!')
+    else:
+        while sweet > 0:
+            player1_step = player_step_check()
+            sweet -= player1_step
+            print(f"{player1} взял {player1_step} конфет, всего осталось {sweet}")
+            if sweet <= 0:
+                print(f'{player2} выйграл')
                 break
-            player2_step = int(input(f'{name2} сколько конфет вы возьмете от 1 до {one_step}: '))
-            sweet -=player2_step
-            print(f"{name2} взял {player2_step} конфет, осталось {sweet}")
-            if sweet == 0:
-                print(f'{name2} проиграл(a)!')
+            if sweet < one_step+2:
+                player2_step = sweet - 1
+            else:
+                player2_step = sweet - (one_step + 2) * (sweet//(one_step + 2))
+
+            sweet -= player2_step
+            print(f'{player2} взял {player2_step} конфет, всего осталось {sweet}')
+            if sweet <= 0:
+                print(f'{player1} выйграл')
                 break
 
-        else:
-            player2_step = int(input(f'{name2} сколько конфет вы возьмете от 1 до {one_step}: '))
-            sweet -=player2_step
-            print(f"{name2} взял {player2_step} конфет, осталось {sweet}")
-            if sweet == 0:
-                print(f'{name2} проиграл(a)!')
+elif choose == 2:
+    print('Вы выбрали режим игры с другим игроком, пожалуйста введите имя игрока 2')
+    name2 = check_name_run()
+    if name1 == name2:
+        name2 += '-2'
+    player1 = name1
+    player2 = name2
+    players = [player1, player2]
+    random.shuffle(players)
+    player1, player2 = players
+    if player1 == name1:
+        while sweet > 0:
+            player1_step = player_step_check()
+            sweet -= player1_step
+            print(f"{player1} взял {player1_step} конфет, всего осталось {sweet}")
+            if sweet <= 0:
+                print(f'{player2} выйграл')
                 break
-            player1_step = int(input(f'{name} сколько конфет вы возьмете от 1 до {one_step}: '))
-            sweet -=player1_step
-            print(f"{name} взял {player1_step} конфет, осталось {sweet}")
-            if sweet == 0:
-                print(f'{name} проиграл(a)!')
+            player2_step = player_step_check()
+            sweet -= player2_step
+            print(f"{player2} взял {player2_step} конфет, всего осталось {sweet}")
+            if sweet <= 0:
+                print(f'{player1} выйграл')
+                break
+    else:
+        while sweet > 0:
+            player2_step = player_step_check()
+            sweet -= player2_step
+            print(f"{player2} взял {player2_step} конфет, всего осталось {sweet}")
+            if sweet <= 0:
+                print(f'{player1} выйграл')
+                break
+            player1_step = player_step_check()
+            sweet -= player1_step
+            print(f"{player1} взял {player1_step} конфет, всего осталось {sweet}")
+            if sweet <= 0:
+                print(f'{player2} выйграл')
                 break
 
-print('Спасибо за игру!')
-        
+elif choose == 3:
+    print('Очень жаль, что не поиграл. Пока')
+    input('\n\nНажмите Enter, чтобы выйти.')
